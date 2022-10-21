27b7f6f97a466e9fd4160aba889dfb2708ac45cd
vikiboss <vikiboss@qq.com>
2022-10-21 21:40:08 +0800

    chore: add prettier rules

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "$schema": "http://json.schemastore.org/prettierrc",
+  // $schema: 'http://json.schemastore.org/prettierrc',
   semi: false,
   singleQuote: true,
-  arrowParens: "avoid",
-  htmlWhitespaceSensitivity: "strict",
+  arrowParens: 'avoid',
+  htmlWhitespaceSensitivity: 'strict',
   editorconfig: true,
 }
